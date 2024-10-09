5eb469d1d8474b3a7149c319637a211f37b65ea0
eugene <eremeeveugene@gmail.com>
2024-10-09 20:46:46 +0300

    Return Length of Arguments Passed

---
+++ source/tests/return-length-of-arguments-passed/return-length-of-arguments-passed.test.js
@@ -0,0 +1,25 @@
+const argumentsLength = require("../../leetcode/return-length-of-arguments-passed/return-length-of-arguments-passed");
+
+describe("argumentsLength", () => {
+  test("should return 0 when no arguments are passed", () => {
+    expect(argumentsLength()).toBe(0);
+  });
+
+  test("should return 1 when one argument is passed", () => {
+    expect(argumentsLength(1)).toBe(1);
+  });
+
+  test("should return 3 when three arguments are passed", () => {
+    expect(argumentsLength(1, 2, 3)).toBe(3);
+  });
+
+  test("should return the correct length with mixed types of arguments", () => {
+    expect(
+      argumentsLength(null, false, 42, "string", [1, 2, 3], { key: "value" })
+    ).toBe(6);
+  });
+
+  test("should return 2 when two arguments are passed", () => {
+    expect(argumentsLength("hello", "world")).toBe(2);
+  });
+});
