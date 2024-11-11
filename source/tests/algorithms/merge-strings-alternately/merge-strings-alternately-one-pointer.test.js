e867d4c61ea5a1c4bd82dc5bf7562fb8882ef6a8
eugene <eremeeveugene@gmail.com>
2024-11-11 21:11:29 +0200

    Merge Strings Alternately

---
+++ source/tests/algorithms/merge-strings-alternately/merge-strings-alternately-one-pointer.test.js
@@ -0,0 +1,4 @@
+const mergeAlternately = require("../../../leetcode/algorithms/merge-strings-alternately/merge-strings-alternately-one-pointer");
+const testMergeAlternately = require("./merge-strings-alternately-tests-base");
+
+testMergeAlternately(mergeAlternately);
